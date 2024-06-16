49d406115acaafb63dd0173c6f58b9b5d647d6cf
willACosta <williamamaral521@gmail.com>
2024-06-16 16:38:24 -0300

    refactor: add astronomy policy

---
+++ astronomy/script.js
@@ -0,0 +1,21 @@
+const themeButton = document.getElementById('theme-switcher');
+const body = document.querySelector("body");
+
+window.addEventListener('load', () => {
+    const theme = localStorage.getItem('theme');
+
+    if (theme) {
+        body.classList.add(theme);
+    }
+});
+
+themeButton.addEventListener('click', () => {
+    if (body.classList.contains('inverse')) {
+        body.classList.remove('inverse');
+        localStorage.removeItem('theme');
+    } else {
+        body.classList.add('inverse');
+        localStorage.setItem('theme', 'inverse');
+    }
+    // body.classList.toggle('dark', true);
+});
