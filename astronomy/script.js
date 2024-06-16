d2175d893b65376b6cf1b434b3e62210b3b2903f
willACosta <williamamaral521@gmail.com>
2024-06-16 16:43:51 -0300

    refactor: improve astronomy page colors

--- astronomy/script.js
+++ astronomy/script.js
@@ -1,21 +1,20 @@
-const themeButton = document.getElementById('theme-switcher');
+const themeButton = document.getElementById("theme-switcher");
 const body = document.querySelector("body");
 
-window.addEventListener('load', () => {
-    const theme = localStorage.getItem('theme');
+window.addEventListener("load", () => {
+  const theme = localStorage.getItem("theme");
 
-    if (theme) {
-        body.classList.add(theme);
-    }
+  if (theme) {
+    body.classList.add(theme);
+  }
 });
 
-themeButton.addEventListener('click', () => {
-    if (body.classList.contains('inverse')) {
-        body.classList.remove('inverse');
-        localStorage.removeItem('theme');
-    } else {
-        body.classList.add('inverse');
-        localStorage.setItem('theme', 'inverse');
-    }
-    // body.classList.toggle('dark', true);
+themeButton.addEventListener("click", () => {
+  if (body.classList.contains("inverse")) {
+    body.classList.remove("inverse");
+    localStorage.removeItem("theme");
+  } else {
+    body.classList.add("inverse");
+    localStorage.setItem("theme", "inverse");
+  }
 });
